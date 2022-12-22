2f84c3d74dcabdd965ed0b90571e83eb7c400b92
ShaharTamir <shahar1360@gmail.com>
2022-12-22 18:14:51 +0200

    Day 57 - Blog based on flask+jinja

---
+++ Day57-BlogJinja/blog_post.py
@@ -0,0 +1,7 @@
+
+class BlogPost:
+
+    def __init__(self, title, prefix, content):
+        self.title = title
+        self.prefix = prefix
+        self.content = content
---
+++ Day57-BlogJinja/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from blog_post import BlogPost
+from datetime import datetime
+app = Flask(__name__)
+blog = []
+year = datetime.now().year
+
+@app.route("/")
+def home_page():
+    global blog
+
+    blog = [
+        BlogPost("Hello", "My pleasure", "content content"),
+        BlogPost("Hello2", "My pleasure2", "content content")
+    ]
+
+    return render_template("index.html", blog_posts=blog, num_posts=len(blog), year=year)
+
+
+@app.route("/blog/<int:num>")
+def get_blog(num):
+    print(blog[num])
+    return render_template("post.html", post=blog[num], year=year)
+
+
+if "__main__" == __name__:
+    app.run(debug=True)
+
