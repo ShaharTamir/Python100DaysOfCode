171e67b3d06be423ad51c4ecc319491eab55f0d9
ShaharTamir <shahar1360@gmail.com>
2022-12-22 18:14:16 +0200

    Day 55 upgrade - adding render template which uses api requests

--- Day55-FlaskGuessNumber/main.py
+++ Day55-FlaskGuessNumber/main.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 from random import randint
+import requests
+from time import sleep
 
 random_number = 0
 app = Flask(__name__)
@@ -32,6 +34,41 @@ def make_guess(number):
     <p style="color:red; font-weight: bold; font-size: 50px;">{string} Guess again...</p>
     <img src="https://media1.giphy.com/media/LSKN6u6u6BWpZRWyyq/giphy.gif?cid=ecf05e476pccwiu1b86ifkutr5jqd8a818aahr2wtq04n9pw&rid=giphy.gif&ct=g">
     """
+
+
+def try_get_url(url, params, header):
+    response = ''
+    while response == '':
+        try:
+            response = requests.get(url, params=params, headers=header)
+        except requests.exceptions.SSLError:
+            print("failed once, try again in 1 sec")
+            sleep(1)
+        else:
+            return response
+
+
+@app.route("/guess/some_name/<name>")
+def send_name(name):
+    gender_api_url = "https://api.genderize.io"
+    age_api_url = "https://api.agify.io"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        "Accept-Language": 'he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7'
+    }
+    api_params = {
+        "name": name
+    }
+
+    gender_response = try_get_url(gender_api_url, api_params, headers)
+    gender_response.raise_for_status()
+    gender = gender_response.json()["gender"]
+
+    age_response = try_get_url(age_api_url, api_params, headers)
+    age_response.raise_for_status()
+    age = age_response.json()["age"]
+
+    return render_template("name_guess.html", name=name, gender=gender, age=age)
 
 
 if __name__ == "__main__":
